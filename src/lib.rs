669bd6de9a5933bf6ee6f3228538878e766ea3ef
Sridhar Ratnakumar <srid@srid.ca>
2023-08-08 17:00:10 -0400

    first log in wasm

--- src/lib.rs
+++ src/lib.rs
@@ -16,21 +16,20 @@ pub fn hydrate() {
     use leptos::*;
     use tracing_subscriber::fmt;
     use tracing_subscriber_wasm::MakeConsoleWriter;
-    // initializes logging using the `log` crate
-    // _ = console_log::init_with_level(log::Level::Debug);
-    // console_error_panic_hook::set_once();
 
     fmt()
-        .with_max_level(tracing::Level::DEBUG)
         .with_writer(
             // To avoide trace events in the browser from showing their
             // JS backtrace, which is very annoying, in my opinion
             MakeConsoleWriter::default().map_trace_level_to(tracing::Level::DEBUG),
         )
+        .with_max_level(tracing::Level::DEBUG)
         // For some reason, if we don't do this in the browser, we get
         // a runtime error.
         .without_time()
         .init();
+
+    tracing::info!("Hydrating app");
 
     leptos::mount_to_body(move |cx| {
         view! { cx, <App/> }
