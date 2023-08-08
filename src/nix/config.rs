49855ec5c5e99fa6a23f59df34b3f9e41d2a303b
Sridhar Ratnakumar <srid@srid.ca>
2023-08-07 22:48:18 -0400

    fmt

--- src/nix/config.rs
+++ src/nix/config.rs
@@ -61,7 +61,12 @@ pub async fn get_nix_config() -> Result<NixConfig, ServerFnError> {
 pub async fn run_nix_show_config() -> Result<NixConfig, ServerFnError> {
     use tokio::process::Command;
     let out = Command::new("nix")
-        .args(vec!["--extra-experimental-features", "nix-command", "show-config", "--json"])
+        .args(vec![
+            "--extra-experimental-features",
+            "nix-command",
+            "show-config",
+            "--json",
+        ])
         .output()
         .await?;
     if out.status.success() {
