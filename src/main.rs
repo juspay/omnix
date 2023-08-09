4e52d405d5ac0158b607090639961d1370beafdb
Sridhar Ratnakumar <srid@srid.ca>
2023-08-09 10:26:09 -0400

    Add a 'main' entrypoint regardless of features

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
+#[cfg(feature = "ssr")]
 #[tokio::main]
 async fn main() {
     nix_browser::server::main().await
+}
+
+#[cfg(not(feature = "ssr"))]
+fn main() {
+    // No main entry point for wasm
 }
