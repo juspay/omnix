f68e8f61ad89037ad7b65711770c73ba7fc403aa
Sridhar Ratnakumar <srid@srid.ca>
2023-08-15 11:40:50 -0400

    dynamic default

--- src/cli.rs
+++ src/cli.rs
@@ -3,6 +3,13 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 pub struct Args {
     /// Do not automatically open the application in the local browser
-    #[arg(short = 'n', long = "no-open", default_value_t = true)]
+    ///
+    /// Enabled by default if the app is running under `cargo leptos ...`
+    #[arg(short = 'n', long = "no-open", default_value_t = in_cargo_leptos())]
     pub no_open: bool,
+}
+
+/// Whether the app is running under `cargo leptos ...`
+fn in_cargo_leptos() -> bool {
+    std::env::var("LEPTOS_OUTPUT_NAME").is_ok()
 }
