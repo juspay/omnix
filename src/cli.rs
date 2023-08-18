1f7691112ab9c542eb62b1c421a44afa07dde43c
Sridhar Ratnakumar <srid@srid.ca>
2023-08-18 11:59:41 -0400

    docs

--- src/cli.rs
+++ src/cli.rs
@@ -8,14 +8,13 @@ pub struct Args {
     /// Enabled by default if the app is running under `cargo leptos ...`
     #[arg(short = 'n', long = "no-open", default_value_t = in_cargo_leptos())]
     pub no_open: bool,
-    /// This flag alters the level log based on count of arg.
-    ///
-    /// 1 -> DEBUG, 2 -> TRACE, Default -> INFO
+    /// Be verbose in server logging (-v, -vv)
     #[arg(short = 'v', long = "verbose", action = clap::ArgAction::Count , default_value_t = 0)]
     pub verbose: u8,
 }
 
 impl Args {
+    /// Return the server log level
     pub fn log_level(&self) -> tracing::Level {
         match self.verbose {
             0 => tracing::Level::INFO,
