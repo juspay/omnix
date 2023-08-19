f7db63bf68427796bde3babf8f4cd2fdab9c6fb9
Sridhar Ratnakumar <srid@srid.ca>
2023-08-19 10:10:44 -0400

    config: substituters is now Url, not String

--- src/nix/health/check/caches.rs
+++ src/nix/health/check/caches.rs
@@ -1,5 +1,6 @@
 use leptos::*;
 use serde::{Deserialize, Serialize};
+use url::Url;
 
 use crate::nix::{
     config::ConfigVal,
@@ -12,7 +13,7 @@ use crate::nix::{
 
 /// Check that [crate::nix::config::NixConfig::substituters] is set to a good value.
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct Caches(ConfigVal<Vec<String>>);
+pub struct Caches(ConfigVal<Vec<Url>>);
 
 impl Check for Caches {
     fn check(info: &info::NixInfo) -> Self {
@@ -23,9 +24,9 @@ impl Check for Caches {
     }
     fn report(&self) -> Report<WithDetails> {
         let val = &self.0.value;
-        if val.contains(&"https://cache.nixos.org".to_string()) {
+        if val.contains(&Url::parse("https://cache.nixos.org").unwrap()) {
             // TODO: Hardcoding this to test failed reports
-            if val.contains(&"https://nammayatri.cachix.org".to_string()) {
+            if val.contains(&Url::parse("https://nammayatri.cachix.org").unwrap()) {
                 Report::Green
             } else {
                 Report::Red(WithDetails {
