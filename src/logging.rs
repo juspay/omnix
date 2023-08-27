7b4090e461f3574c6f62e71f820b793241e0bef5
Vipul Gupta <73114964+1vipulgupta@users.noreply.github.com>
2023-08-28 00:29:38 +0530

    cli: add verbosity option (`-vv...`) (#41)

--- src/logging.rs
+++ src/logging.rs
@@ -5,12 +5,16 @@ use tower_http::{
     classify::{ServerErrorsAsFailures, SharedClassifier},
     trace::TraceLayer,
 };
+#[cfg(feature = "ssr")]
+use tracing_subscriber::filter::{Directive, LevelFilter};
+#[cfg(feature = "ssr")]
+use tracing_subscriber::EnvFilter;
 
 /// Setup server-side logging using [tracing_subscriber]
 #[cfg(feature = "ssr")]
-pub fn setup_server_logging() {
+pub fn setup_server_logging(verbosity: &Verbosity) {
     tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::INFO) // TODO: --verbose should use DEBUG
+        .with_env_filter(verbosity.log_filter())
         .compact()
         .init();
 }
@@ -39,6 +43,51 @@ pub fn http_trace_layer() -> TraceLayer<SharedClassifier<ServerErrorsAsFailures>
     use tracing::Level;
 
     TraceLayer::new_for_http()
-        .make_span_with(trace::DefaultMakeSpan::new().level(Level::DEBUG))
-        .on_response(trace::DefaultOnResponse::new().level(Level::DEBUG))
+        .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
+        .on_response(trace::DefaultOnResponse::new().level(Level::INFO))
+}
+
+#[derive(clap::Args, Debug, Clone)]
+pub struct Verbosity {
+    /// Server logging level
+    ///
+    /// Pass multiple v's (`-vvv...`) to increase logging level.
+    #[arg(short = 'v', long = "verbose", action = clap::ArgAction::Count, default_value_t = 0)]
+    pub verbose: u8,
+}
+
+#[cfg(feature = "ssr")]
+impl Verbosity {
+    /// Return the log filter for CLI flag.
+    fn log_filter(&self) -> EnvFilter {
+        self.log_directives().iter().fold(
+            EnvFilter::from_env("NIX_BROWSER_LOG"),
+            |filter, directive| filter.add_directive(directive.clone()),
+        )
+    }
+
+    fn log_directives(&self) -> Vec<Directive> {
+        // Allow warnings+errors from all crates.
+        let base_filter = LevelFilter::WARN.into();
+        match self.verbose {
+            // Default
+            0 => vec![base_filter, "nix_browser=info".parse().unwrap()],
+            // -v: log app DEBUG level, as well as http requests
+            1 => vec![
+                base_filter,
+                "nix_browser=debug".parse().unwrap(),
+                "tower_http=info".parse().unwrap(),
+            ],
+            // -vv: log app TRACE level, as well as http requests
+            2 => vec![
+                base_filter,
+                "nix_browser=trace".parse().unwrap(),
+                "tower_http=info".parse().unwrap(),
+            ],
+            // -vvv: log DEBUG level of app and libraries
+            3 => vec![LevelFilter::DEBUG.into()],
+            // -vvvv: log TRACE level of app and libraries
+            _ => vec![LevelFilter::TRACE.into()],
+        }
+    }
 }
