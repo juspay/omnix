51ac4627ff78e66edf915f7e8cdc6b2007aa630f
Sridhar Ratnakumar <srid@srid.ca>
2023-08-31 11:57:36 -0400

    view as well

--- crates/nix_rs/src/info.rs
+++ crates/nix_rs/src/info.rs
@@ -1,5 +1,4 @@
 //! Information about the user's Nix installation
-use leptos::*;
 use serde::{Deserialize, Serialize};
 
 use crate::{config::NixConfig, version::NixVersion};
@@ -15,34 +14,14 @@ pub struct NixInfo {
 impl NixInfo {
     /// Determine [NixInfo] on the user's system
     #[cfg(feature = "ssr")]
-    pub async fn from_nix(nix_cmd: &crate::command::NixCmd) -> Result<NixInfo, ServerFnError> {
+    pub async fn from_nix(
+        nix_cmd: &crate::command::NixCmd,
+    ) -> Result<NixInfo, crate::command::NixCmdError> {
         let nix_version = NixVersion::from_nix(nix_cmd).await?;
         let nix_config = NixConfig::from_nix(nix_cmd).await?;
         Ok(NixInfo {
             nix_version,
             nix_config,
         })
-    }
-}
-
-impl IntoView for NixInfo {
-    fn into_view(self, cx: Scope) -> View {
-        view! { cx,
-            <div class="flex flex-col p-4 space-y-8 bg-white border-2 rounded border-base-400">
-                <div>
-                    <b>
-                        Nix Version
-                    </b>
-                    <div class="p-1 my-1 rounded bg-primary-50">{self.nix_version}</div>
-                </div>
-                <div>
-                    <b>
-                        Nix Config
-                    </b>
-                    {self.nix_config}
-                </div>
-            </div>
-        }
-        .into_view(cx)
     }
 }
--- src/app/info.rs
+++ src/app/info.rs
@@ -1,6 +1,7 @@
 //! Frontend UI entry point
 
 use leptos::*;
+use leptos_extra::signal::SignalWithResult;
 use leptos_meta::*;
 use nix_rs::info::NixInfo;
 
@@ -18,7 +19,14 @@ pub fn NixInfoRoute(cx: Scope) -> impl IntoView {
         <h1 class="text-5xl font-bold">{title}</h1>
         <RefetchQueryButton result query=|| ()/>
         <div class="my-1 text-left">
-            <SuspenseWithErrorHandling>{data}</SuspenseWithErrorHandling>
+            <SuspenseWithErrorHandling>
+                {move || {
+                    data.with_result(move |info| {
+                        view! { cx, <NixInfoView info/> }
+                    })
+                }}
+
+            </SuspenseWithErrorHandling>
         </div>
     }
 }
@@ -28,4 +36,24 @@ pub fn NixInfoRoute(cx: Scope) -> impl IntoView {
 pub async fn get_nix_info(_unit: ()) -> Result<NixInfo, ServerFnError> {
     let v = NixInfo::from_nix(&nix_rs::command::NixCmd::default()).await?;
     Ok(v)
+}
+
+#[component]
+fn NixInfoView<'a>(cx: Scope, info: &'a NixInfo) -> impl IntoView {
+    view! { cx,
+        <div class="flex flex-col p-4 space-y-8 bg-white border-2 rounded border-base-400">
+            <div>
+                <b>
+                    Nix Version
+                </b>
+                <div class="p-1 my-1 rounded bg-primary-50">{info.nix_version.clone()}</div>
+            </div>
+            <div>
+                <b>
+                    Nix Config
+                </b>
+                {info.nix_config.clone()}
+            </div>
+        </div>
+    }
 }
