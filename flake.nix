27600e01834d3e5570eef0c1ae6ed052251acad8
Naman Agarwal <naman.agarwal@juspay.in>
2023-08-31 16:47:13 +0530

    fix flake formatting

--- flake.nix
+++ flake.nix
@@ -27,57 +27,56 @@
         (inputs.leptos-fullstack + /nix/flake-module.nix)
         ./nix/e2e.nix
       ];
-      perSystem = { config, self', pkgs, lib, system, ... }:
-        {
-          _module.args.pkgs = import inputs.nixpkgs {
-            inherit system;
-            overlays = [
-              inputs.rust-overlay.overlays.default
-            ];
-          };
-
-          # Add your auto-formatters here.
-          # cf. https://numtide.github.io/treefmt/
-          treefmt.config = {
-            projectRootFile = "flake.nix";
-            programs = {
-              nixpkgs-fmt.enable = true;
-              rustfmt.enable = true;
-              leptosfmt.enable = true;
-            };
-          };
-
-          leptos-fullstack.overrideCraneArgs = oa: {
-            SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
-            nativeBuildInputs = (oa.nativeBuildInputs or [ ]) ++ [
-              pkgs.nix # cargo tests need nix
-            ];
-            # Disable tests on macOS for https://github.com/garnix-io/issues/issues/69
-            # If/when we move to Jenkins, this won't be necessary.
-            doCheck = !pkgs.stdenv.isDarwin;
-            meta.description = "WIP: nix-browser";
-          };
-
-          packages.default = self'.packages.nix-browser;
-
-          devShells.default = pkgs.mkShell ({
-            inputsFrom = [
-              config.treefmt.build.devShell
-              self'.devShells.nix-browser
-            ];
-            packages = with pkgs; [
-              just
-              nixci
-              cargo-watch
-              cargo-expand
-              config.process-compose.cargo-doc-live.outputs.package
-            ];
-            shellHook = ''
-              echo
-              echo "🍎🍎 Run 'just <recipe>' to get started"
-              just
-            '';
-          });
+      perSystem = { config, self', pkgs, lib, system, ... }: {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            inputs.rust-overlay.overlays.default
+          ];
         };
+
+        # Add your auto-formatters here.
+        # cf. https://numtide.github.io/treefmt/
+        treefmt.config = {
+          projectRootFile = "flake.nix";
+          programs = {
+            nixpkgs-fmt.enable = true;
+            rustfmt.enable = true;
+            leptosfmt.enable = true;
+          };
+        };
+
+        leptos-fullstack.overrideCraneArgs = oa: {
+          SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
+          nativeBuildInputs = (oa.nativeBuildInputs or [ ]) ++ [
+            pkgs.nix # cargo tests need nix
+          ];
+          # Disable tests on macOS for https://github.com/garnix-io/issues/issues/69
+          # If/when we move to Jenkins, this won't be necessary.
+          doCheck = !pkgs.stdenv.isDarwin;
+          meta.description = "WIP: nix-browser";
+        };
+
+        packages.default = self'.packages.nix-browser;
+
+        devShells.default = pkgs.mkShell ({
+          inputsFrom = [
+            config.treefmt.build.devShell
+            self'.devShells.nix-browser
+          ];
+          packages = with pkgs; [
+            just
+            nixci
+            cargo-watch
+            cargo-expand
+            config.process-compose.cargo-doc-live.outputs.package
+          ];
+          shellHook = ''
+            echo
+            echo "🍎🍎 Run 'just <recipe>' to get started"
+            just
+          '';
+        });
+      };
     };
 }
