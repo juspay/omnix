370e2572b68545f3248b02b7723a4516b2ee0147
Sridhar Ratnakumar <srid@srid.ca>
2023-09-06 11:50:10 -0400

    Improve cli output

--- crates/nix_health/src/main.rs
+++ crates/nix_health/src/main.rs
@@ -1,13 +1,27 @@
-use nix_health::{traits::Check, NixHealth};
+use anyhow::Context;
+use colored::Colorize;
+use nix_health::{report::Report, traits::Check, NixHealth};
 use nix_rs::{command::NixCmd, info::NixInfo};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     human_panic::setup_panic!();
-    let info = NixInfo::from_nix(&NixCmd::default()).await?;
+    let info = NixInfo::from_nix(&NixCmd::default())
+        .await
+        .with_context(|| "Unable to gather nix info")?;
     let health = NixHealth::check(&info);
     for check in &health {
-        println!("{}: {:?}", check.name(), check.report());
+        let report = check.report();
+        match report {
+            Report::Green => {
+                println!("{}", format!("✅ {}", check.name()).green().bold());
+            }
+            Report::Red(details) => {
+                println!("{}", format!("❌ {}", check.name()).red().bold());
+                println!("   {}", details.msg.yellow());
+                println!("   {}", details.suggestion);
+            }
+        }
     }
     Ok(())
 }
