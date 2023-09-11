905acf48548e0ad4d8133bba31da2f33849012f6
Sridhar Ratnakumar <srid@srid.ca>
2023-09-11 16:11:51 -0400

    reindent

--- e2e-playwright/flake-module.nix
+++ e2e-playwright/flake-module.nix
@@ -1,77 +1,76 @@
 { lib, ... }:
 {
-  perSystem = { config, self', pkgs, system, ... }:
-    {
-      # e2e test service using playwright
-      process-compose.e2e-playwright-test =
-        let
-          TEST_PORT = "5000";
-          PLAYWRIGHT_BROWSERS_PATH = "${pkgs.playwright-driver.browsers}";
-        in
-        {
-          tui = false;
-          port = 8975;
-          settings.processes = {
-            start-app = {
-              command = "${lib.getExe self'.packages.default} --site-addr=127.0.0.1:${TEST_PORT} --no-open";
-              readiness_probe = {
-                exec.command = "${lib.getExe pkgs.curl} --fail 127.0.0.1:${TEST_PORT}";
-                initial_delay_seconds = 2;
-                period_seconds = 10;
-                timeout_seconds = 4;
-              };
+  perSystem = { config, self', pkgs, system, ... }: {
+    # e2e test service using playwright
+    process-compose.e2e-playwright-test =
+      let
+        TEST_PORT = "5000";
+        PLAYWRIGHT_BROWSERS_PATH = "${pkgs.playwright-driver.browsers}";
+      in
+      {
+        tui = false;
+        port = 8975;
+        settings.processes = {
+          start-app = {
+            command = "${lib.getExe self'.packages.default} --site-addr=127.0.0.1:${TEST_PORT} --no-open";
+            readiness_probe = {
+              exec.command = "${lib.getExe pkgs.curl} --fail 127.0.0.1:${TEST_PORT}";
+              initial_delay_seconds = 2;
+              period_seconds = 10;
+              timeout_seconds = 4;
             };
-            test = {
-              environment = {
-                inherit TEST_PORT;
-              };
-              command = pkgs.writeShellApplication {
-                name = "e2e-playwright";
-                runtimeInputs = with pkgs; [ nodejs playwright-test PLAYWRIGHT_BROWSERS_PATH ];
-                text = ''
-                  cd e2e-playwright
-                  playwright test --project chromium
-                '';
-              };
-              depends_on."start-app".condition = "process_healthy";
-              availability.exit_on_end = true;
+          };
+          test = {
+            environment = {
+              inherit TEST_PORT;
             };
+            command = pkgs.writeShellApplication {
+              name = "e2e-playwright";
+              runtimeInputs = with pkgs; [ nodejs playwright-test PLAYWRIGHT_BROWSERS_PATH ];
+              text = ''
+                cd e2e-playwright
+                playwright test --project chromium
+              '';
+            };
+            depends_on."start-app".condition = "process_healthy";
+            availability.exit_on_end = true;
           };
         };
+      };
 
-      devShells.e2e-playwright =
-        let
-          nodeModules = pkgs.buildNpmPackage {
-            pname = "e2e-playwright";
-            version = "1.0.0";
-            src = builtins.path {
-              path = ./.;
-              filter = path: _:
-                let name = builtins.baseNameOf path; in
-                name == "package.json" ||
-                name == "package-lock.json" ||
-                name == "tests" ||
-                name == "playwright.config.js";
-            };
-            npmDepsHash = "sha256-WAMYkjC00ReCyyh5uI8vV7HJ+BEclBlxvBgEn9Mphe0=";
-            # npmDepsHash = lib.fakeHash;
-            dontBuild = true;
-            installPhase = ''
-              cp -r node_modules $out/
-            '';
+    devShells.e2e-playwright =
+      let
+        nodeModules = pkgs.buildNpmPackage {
+          pname = "e2e-playwright";
+          version = "1.0.0";
+          src = builtins.path {
+            path = ./.;
+            filter = path: _:
+              let name = builtins.baseNameOf path; in
+              name == "package.json" ||
+              name == "package-lock.json" ||
+              name == "tests" ||
+              name == "playwright.config.js";
           };
-        in
-        pkgs.mkShell {
-          buildInputs = with pkgs; [
-            nodejs
-            playwright-test
-          ];
-          shellHook = ''
-            export NODE_PATH=${nodeModules}
-            # VSCode disrespects NODE_PATH https://github.com/microsoft/TypeScript/issues/8760
-            # So we must manually create ./node_modules
-            just node_modules NODE_PATH=$NODE_PATH
+          npmDepsHash = "sha256-WAMYkjC00ReCyyh5uI8vV7HJ+BEclBlxvBgEn9Mphe0=";
+          # npmDepsHash = lib.fakeHash;
+          dontBuild = true;
+          installPhase = ''
+            cp -r node_modules $out/
           '';
         };
-    };
+      in
+      pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nodejs
+          playwright-test
+        ];
+        shellHook = ''
+          export NODE_PATH=${nodeModules}
+          # VSCode disrespects NODE_PATH https://github.com/microsoft/TypeScript/issues/8760
+          # So we must manually create ./node_modules
+          just node_modules NODE_PATH=$NODE_PATH
+        '';
+      };
+  };
 }
