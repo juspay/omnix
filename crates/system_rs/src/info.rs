202e443ad4a8103d44b392876f870a82bcc1d9fb
shivaraj-bh <sbh69840@gmail.com>
2023-09-12 19:29:27 +0530

    fix: system_rs doc

--- crates/system_rs/src/info.rs
+++ crates/system_rs/src/info.rs
@@ -1,15 +1,15 @@
-//! Information about the user's System
-use os_info;
+//! Information about the user's system
 use serde::{Deserialize, Serialize};
 #[cfg(feature = "all")]
 use std::env;
+use os_info;
 
-/// All the information about the user's Nix installation
+/// Information about the user's system
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct SysInfo {
     /// value of $USER
     pub current_user: String,
-    pub os: os_info::Type,
+    pub os: os_info::Type
 }
 
 impl SysInfo {
@@ -18,6 +18,9 @@ impl SysInfo {
     pub async fn get_info() -> Result<SysInfo, env::VarError> {
         let current_user = env::var("USER")?;
         let os = os_info::get().os_type();
-        Ok(SysInfo { current_user, os })
+        Ok(SysInfo {
+            current_user,
+            os,
+        })
     }
 }
