a9784d949ae7af42698669904a4f596021c1096b
shivaraj-bh <sbh69840@gmail.com>
2023-09-13 17:02:35 +0530

    pass sys_info in nix_health's main

--- crates/nix_health/src/main.rs
+++ crates/nix_health/src/main.rs
@@ -1,15 +1,18 @@
 use anyhow::Context;
 use colored::Colorize;
 use nix_health::{report::Report, traits::Check, NixHealth};
-use nix_rs::{command::NixCmd, info::NixInfo};
+use nix_rs::{command::NixCmd, info::NixInfo, system::SysInfo};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     human_panic::setup_panic!();
-    let info = NixInfo::from_nix(&NixCmd::default())
+    let nix_info = NixInfo::from_nix(&NixCmd::default())
         .await
         .with_context(|| "Unable to gather nix info")?;
-    let health = NixHealth::check(&info);
+    let sys_info = SysInfo::get_info()
+        .await
+        .with_context(|| "Unable to gather system info")?;
+    let health = NixHealth::check(&nix_info, &sys_info);
     println!("Checking the health of your Nix setup:\n");
     for check in &health {
         let report = check.report();
