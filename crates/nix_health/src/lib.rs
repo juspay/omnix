5011ac641e2f25067dea6195db63a12ab78f16e7
Sridhar Ratnakumar <srid@srid.ca>
2023-09-15 16:02:36 -0400

    nix_health: Add more tests

--- crates/nix_health/src/lib.rs
+++ crates/nix_health/src/lib.rs
@@ -87,10 +87,21 @@ mod tests {
     }
 
     #[test]
-    fn test_json_deserialize_some() {
+    fn test_json_deserialize_nix_version() {
         let json = r#"{ "nix-version": { "min-required": "2.17.0" } }"#;
         let v: super::NixHealth = serde_json::from_str(json).unwrap();
         assert_eq!(v.nix_version.min_required.to_string(), "2.17.0");
         assert_eq!(v.caches, Caches::default());
+    }
+
+    #[test]
+    fn test_json_deserialize_caches() {
+        let json = r#"{ "caches": { "required": ["https://foo.cachix.org"] } }"#;
+        let v: super::NixHealth = serde_json::from_str(json).unwrap();
+        assert_eq!(
+            v.caches.required,
+            vec![url::Url::parse("https://foo.cachix.org").unwrap()]
+        );
+        assert_eq!(v.nix_version, MinNixVersion::default());
     }
 }
