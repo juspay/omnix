edb1fc0a28803e39e36c8bfd554f65d7afdcd7eb
Sridhar Ratnakumar <srid@srid.ca>
2023-09-15 13:57:38 -0400

    Fix caches check json config

--- crates/nix_health/README.md
+++ crates/nix_health/README.md
@@ -1,1 +1,8 @@
-Next-gen version of <https://github.com/srid/nix-health> in progress.
+Next-gen version of <https://github.com/srid/nix-health> in progress. https://github.com/juspay/nix-browser/issues/14
+
+To try it out,
+
+```
+cd /to/your/project/with/flake
+nix run github:juspay/nix-browser#nix-health
+```
--- crates/nix_health/src/check/caches.rs
+++ crates/nix_health/src/check/caches.rs
@@ -9,13 +9,13 @@ use crate::traits::*;
 #[serde(rename_all = "kebab-case")]
 pub struct Caches {
     #[serde(default = "default_caches")]
-    pub required_caches: Vec<Url>,
+    pub required: Vec<Url>,
 }
 
 impl Default for Caches {
     fn default() -> Self {
         Caches {
-            required_caches: default_caches(),
+            required: default_caches(),
         }
     }
 }
@@ -27,13 +27,13 @@ fn default_caches() -> Vec<Url> {
 impl Checkable for Caches {
     fn check(&self, nix_info: &info::NixInfo, _nix_env: &env::NixEnv) -> Option<Check> {
         let val = &nix_info.nix_config.substituters.value;
-        let result = if self.required_caches.iter().all(|c| val.contains(c)) {
+        let result = if self.required.iter().all(|c| val.contains(c)) {
             CheckResult::Green
         } else {
             CheckResult::Red {
                 msg: format!(
                     "You are missing a required cache: {}",
-                    self.required_caches
+                    self.required
                         .iter()
                         .find(|required_cache| !val.contains(required_cache))
                         .unwrap()
