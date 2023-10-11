b267e2ebaa11a2662854dc16dc02b7eecaa43bf3
Sridhar Ratnakumar <srid@srid.ca>
2023-10-11 19:10:24 -0400

    trim

--- rust.nix
+++ rust.nix
@@ -4,35 +4,12 @@
 {
   perSystem = { config, self', pkgs, lib, system, ... }: {
     dioxus-desktop = {
-      overrideCraneArgs = oa:
-        let
-          # 'cargo leptos test' doesn't run tests for all crates in the
-          # workspace. We do it here.
-          run-test = pkgs.writeShellApplication {
-            name = "run-test";
-            text = ''
-              set -xe
-
-              ${oa.cargoTestCommand or ""}
-
-              # Disable tests on macOS for https://github.com/garnix-io/issues/issues/69
-              # If/when we move to Jenkins, this won't be necessary.
-              ${if !pkgs.stdenv.isDarwin
-                then ''
-                  cargo test 
-                ''
-                else ""
-              }
-            '';
-          };
-        in
-        {
-          nativeBuildInputs = (oa.nativeBuildInputs or [ ]) ++ [
-            pkgs.nix # cargo tests need nix
-          ];
-          # cargoTestCommand = lib.getExe run-test;
-          meta.description = "WIP: nix-browser";
-        };
+      overrideCraneArgs = oa: {
+        nativeBuildInputs = (oa.nativeBuildInputs or [ ]) ++ [
+          pkgs.nix # cargo tests need nix
+        ];
+        meta.description = "WIP: nix-browser";
+      };
     };
 
     packages = {
