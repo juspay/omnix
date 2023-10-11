a7290bcdaf98ccc1d70957799065eef461138ff3
Sridhar Ratnakumar <srid@srid.ca>
2023-10-11 18:54:24 -0400

    nix_env too

--- src/app/state.rs
+++ src/app/state.rs
@@ -36,7 +36,7 @@ impl From<String> for SystemError {
 #[derive(Default, Clone, Copy, Debug)]
 pub struct AppState {
     pub nix_info: Signal<Datum<Result<nix_rs::info::NixInfo, SystemError>>>,
-    pub nix_env: Signal<Option<Result<nix_rs::env::NixEnv, SystemError>>>,
+    pub nix_env: Signal<Datum<Result<nix_rs::env::NixEnv, SystemError>>>,
     pub health_checks: Signal<Datum<Result<Vec<nix_health::traits::Check>, SystemError>>>,
 
     pub flake_url: Signal<FlakeUrl>,
@@ -73,18 +73,18 @@ impl AppState {
     #[instrument(name = "update-nix-env", skip(self))]
     pub async fn update_nix_env(&self) {
         tracing::info!("Updating nix env ...");
-        let nix_env = tokio::spawn(async move {
-            nix_rs::env::NixEnv::detect(None)
-                .await
-                .map_err(|e| e.to_string().into())
+        Datum::refresh_with(self.nix_env, async {
+            let nix_env = tokio::spawn(async move {
+                nix_rs::env::NixEnv::detect(None)
+                    .await
+                    .map_err(|e| e.to_string().into())
+            })
+            .await
+            .unwrap();
+            tracing::info!("Got nix env, about to mut");
+            nix_env
         })
-        .await
-        .unwrap();
-        tracing::info!("Got nix env, about to mut");
-        self.nix_env.with_mut(move |x| {
-            *x = Some(nix_env);
-            tracing::info!("Updated nix env");
-        });
+        .await;
     }
 
     #[instrument(name = "update-health-checks", skip(self))]
@@ -97,7 +97,7 @@ impl AppState {
             let get_nix_health = move || -> Result<Vec<nix_health::traits::Check>, SystemError> {
                 let nix_env = self.nix_env.read();
                 let nix_env: &nix_rs::env::NixEnv = nix_env
-                    .as_ref()
+                    .current_value()
                     .unwrap()
                     .as_ref()
                     .map_err(|e| Into::<SystemError>::into(e.to_string()))?;
