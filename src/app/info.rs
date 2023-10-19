bed0e34f73a83c003d01fb4660a16da21aaa2358
Sridhar Ratnakumar <srid@srid.ca>
2023-10-19 13:57:47 -0400

    Render NixEnv under info tab

--- src/app/info.rs
+++ src/app/info.rs
@@ -3,7 +3,7 @@
 use std::fmt::Display;
 
 use dioxus::prelude::*;
-use nix_rs::{config::NixConfig, info::NixInfo, version::NixVersion};
+use nix_rs::{config::NixConfig, env::NixEnv, info::NixInfo, version::NixVersion};
 
 use crate::{
     app::state::AppState,
@@ -24,14 +24,16 @@ pub fn Info(cx: Scope) -> Element {
                 state.act(Action::GetNixInfo);
             }
         }
-        nix_info.render_with(cx, |v| render! { NixInfoView { info: v.clone() } })
+        div { class: "flex items-center justify-center",
+            nix_info.render_with(cx, |v| render! { NixInfoView { info: v.clone() } })
+        }
     }
 }
 
 #[component]
 fn NixInfoView(cx: Scope, info: NixInfo) -> Element {
     render! {
-        div { class: "flex flex-col p-4 space-y-8 bg-white border-2 rounded border-base-400",
+        div { class: "flex flex-col max-w-prose p-4 space-y-8 bg-white border-2 rounded border-base-400",
             div {
                 b { "Nix Version" }
                 div { class: "p-1 my-1 rounded bg-primary-50", NixVersionView { version: info.nix_version } }
@@ -39,6 +41,10 @@ fn NixInfoView(cx: Scope, info: NixInfo) -> Element {
             div {
                 b { "Nix Config" }
                 NixConfigView { config: info.nix_config.clone() }
+            }
+            div {
+                b { "Nix Env" }
+                NixEnvView { env: info.nix_env.clone() }
             }
         }
     }
@@ -51,40 +57,14 @@ fn NixVersionView(cx: Scope, version: NixVersion) -> Element {
 
 #[component]
 fn NixConfigView(cx: Scope, config: NixConfig) -> Element {
-    let config_row = |key: &'static str, title: String, children: Element<'a>| {
-        render! {
-            tr { title: "{title}",
-                td { class: "px-4 py-2 font-semibold text-base-700", "{key}" }
-                td { class: "px-4 py-2 text-left",
-                    code { children }
-                }
-            }
-        }
-    };
     render! {
         div { class: "py-1 my-1 rounded bg-primary-50",
             table { class: "text-right",
                 tbody {
-                    config_row (
-                        "Local System",
-                        config.system.description.clone(),
-                        render! { "{config.system.value}" }
-                    ),
-                    config_row (
-                        "Max Jobs",
-                        config.max_jobs.description.clone(),
-                        render! {"{config.max_jobs.value}"}
-                    ),
-                    config_row (
-                        "Cores per build",
-                        config.cores.description.clone(),
-                        render! { "{config.cores.value}" }
-                    ),
-                    config_row (
-                        "Nix Caches",
-                        config.substituters.clone().description,
-                        render! { ConfigValList { items: &config.substituters.value } }
-                    )
+                    TableRow { name: "Local System", title: &config.system.description, "{config.system.value}" }
+                    TableRow { name: "Max Jobs", title: &config.max_jobs.description, "{config.max_jobs.value}" }
+                    TableRow { name: "Cores per build", title: &config.cores.description, "{config.cores.value}" }
+                    TableRow { name: "Nix Caches", title: &config.substituters.description, ConfigValList { items: &config.substituters.value } }
                 }
             }
         }
@@ -100,6 +80,34 @@ where
         div { class: "flex flex-col space-y-4",
             for item in items {
                 li { class: "list-disc", "{item}" }
+            }
+        }
+    }
+}
+
+#[component]
+fn NixEnvView(cx: Scope, env: NixEnv) -> Element {
+    render! {
+        div { class: "py-1 my-1 rounded bg-primary-50",
+            table { class: "text-right",
+                tbody {
+                    TableRow { name: "Current User", title: "Logged-in user", code { "{env.current_user}" } }
+                    TableRow { name: "OS", title: "Operating System", code { "{env.os}" } }
+                    TableRow { name: "Total disk space", title: "Total disk space on the current machine", code { "{env.total_disk_space}" } }
+                    TableRow { name: "Total RAM", title: "Total memory on the current machine", code { "{env.total_memory}" } }
+                }
+            }
+        }
+    }
+}
+
+#[component]
+fn TableRow<'a>(cx: Scope, name: &'static str, title: &'a str, children: Element<'a>) -> Element {
+    render! {
+        tr { title: "{title}",
+            td { class: "px-4 py-2 font-semibold text-base-700", "{name}" }
+            td { class: "px-4 py-2 text-left",
+                code { children }
             }
         }
     }
