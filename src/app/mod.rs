65021a39e48c248390148907fcba6a290ec2b15d
Sridhar Ratnakumar <srid@srid.ca>
2023-10-24 18:34:09 -0400

    display real data in top bar

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -40,21 +40,60 @@ enum Route {
 pub fn App(cx: Scope) -> Element {
     AppState::provide_state(cx);
     render! {
-        body {
-            div { class: "flex flex-col text-center justify-between w-full overflow-hidden h-screen  bg-base-200",
-                Router::<Route> {}
-            }
-        }
+        body { Router::<Route> {} }
     }
 }
 
 fn Wrapper(cx: Scope) -> Element {
     render! {
-        Nav {}
-        Scrollable {
-            div { class: "m-2 py-2", Outlet::<Route> {} }
+        div { class: "flex flex-col text-center justify-between w-full overflow-hidden h-screen bg-base-100",
+            div {
+                TopBar {}
+                Nav {}
+                Scrollable {
+                    div { class: "m-2 py-2", Outlet::<Route> {} }
+                }
+            }
+            Footer {}
         }
-        Footer {}
+    }
+}
+
+#[component]
+fn TopBar(cx: Scope) -> Element {
+    let state = AppState::use_state(cx);
+    let health_checks = state.health_checks.read();
+    let nix_info = state.nix_info.read();
+    render! {
+        div { class: "flex justify-between items-center w-full p-2",
+            Link { to: Route::Health {},
+                span { title: "Nix Health Status",
+                    match (*health_checks).current_value() {
+                        Some(Ok(checks)) => render! {
+                            if checks.iter().all(|check| check.result.green()) {
+                                "✅"
+                            } else {
+                                "❌"
+                            }
+                        },
+                        Some(Err(err)) => render! { "{err}" },
+                        None => render! { Loader {} },
+                    }
+                }
+            }
+            Link { to: Route::Info {},
+                span {
+                    "ℹ️ Nix "
+                    match (*nix_info).current_value() {
+                        Some(Ok(info)) => render! {
+                            "{info.nix_version} on {info.nix_env.os}"
+                        },
+                        Some(Err(err)) => render! { "{err}" },
+                        None => render! { Loader {} },
+                    }
+                }
+            }
+        }
     }
 }
 
