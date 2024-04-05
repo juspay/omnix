ac501e27860f89e758b9c7f15613a1578b6b90dc
Sridhar Ratnakumar <srid@srid.ca>
2024-04-05 12:41:13 +1100

    rm ./crates
    They have been moved to other repos

--- crates/nix_health/src/check/mod.rs
+++
@@ -1,9 +0,0 @@
-//! Individual Nix checks
-pub mod caches;
-pub mod direnv;
-pub mod flake_enabled;
-pub mod max_jobs;
-pub mod min_nix_version;
-pub mod rosetta;
-pub mod system;
-pub mod trusted_users;
--- crates/nix_rs/src/refs.rs
+++
@@ -1,5 +0,0 @@
-//! Links to Nix manual and other documentation
-
-/// Link to information about the various Nix versions
-pub const RELEASE_HISTORY: &str =
-    "https://nixos.org/manual/nix/stable/release-notes/release-notes.html";
