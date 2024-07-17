9d1da5d54fcba79faf383e2b6308351458aabae3
shivaraj-bh <sbh69840@gmail.com>
2024-07-18 00:19:02 +0530

    omnix-cli: fmt

--- crates/omnix-cli/tests/cli.rs
+++ crates/omnix-cli/tests/cli.rs
@@ -29,7 +29,11 @@ fn om_show_local() -> anyhow::Result<()> {
 /// `om show` runs, and succeeds for a remote flake.
 #[test]
 fn om_show_remote() -> anyhow::Result<()> {
-    om()?.arg("show").arg("github:srid/haskell-multi-nix/c85563721c388629fa9e538a1d97274861bc8321").assert().success();
+    om()?
+        .arg("show")
+        .arg("github:srid/haskell-multi-nix/c85563721c388629fa9e538a1d97274861bc8321")
+        .assert()
+        .success();
     Ok(())
 }
 
