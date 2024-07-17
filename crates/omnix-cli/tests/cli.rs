cb669c6e6d023cf4c0a56649366ecb7ac56e4cb2
shivaraj-bh <sbh69840@gmail.com>
2024-07-18 00:17:22 +0530

    omnix-cli: add integration tests

--- crates/omnix-cli/tests/cli.rs
+++ crates/omnix-cli/tests/cli.rs
@@ -19,6 +19,20 @@ fn om_health() -> anyhow::Result<()> {
     Ok(())
 }
 
+/// `om show` runs, and succeeds for a local flake.
+#[test]
+fn om_show_local() -> anyhow::Result<()> {
+    om()?.arg("show").arg(".").assert().success();
+    Ok(())
+}
+
+/// `om show` runs, and succeeds for a remote flake.
+#[test]
+fn om_show_remote() -> anyhow::Result<()> {
+    om()?.arg("show").arg("github:srid/haskell-multi-nix/c85563721c388629fa9e538a1d97274861bc8321").assert().success();
+    Ok(())
+}
+
 /// Return the [Command] pointing to the `om` cargo bin
 fn om() -> anyhow::Result<Command> {
     Ok(Command::cargo_bin("om")?)
