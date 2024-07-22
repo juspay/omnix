4618cc65233789b2767de2c7f59c9340fffcd986
Sridhar Ratnakumar <srid@srid.ca>
2024-07-22 15:16:41 -0400

    init: Hook flakreate to `om init`

--- crates/omnix-cli/src/command/init.rs
+++ crates/omnix-cli/src/command/init.rs
@@ -1,12 +1,28 @@
+use std::{env::var, path::PathBuf, sync::LazyLock};
+
 use clap::Parser;
+use nix_rs::flake::url::FlakeUrl;
+
+static REGISTRY: LazyLock<FlakeUrl> =
+    LazyLock::new(|| PathBuf::from(var("FLAKREATE_REGISTRY").unwrap()).into());
 
 /// Initialize a new flake project
 #[derive(Parser, Debug)]
-pub struct InitConfig {}
+pub struct InitConfig {
+    /// Flake template registry to use
+    ///
+    /// The flake attribute is treated as a glob pattern to select the
+    /// particular template (or subset of templates) to use.
+    #[arg(short = 't', default_value_t = REGISTRY.clone())]
+    registry: FlakeUrl,
+
+    /// Where to create the template
+    #[arg()]
+    path: PathBuf,
+}
 
 impl InitConfig {
     pub async fn run(&self) -> anyhow::Result<()> {
-        println!("TODO(om init)");
-        Ok(())
+        flakreate::flakreate(self.registry.clone(), self.path.clone()).await
     }
 }
