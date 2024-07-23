51e955b2fd85f130efdd18b99392963a7b9ff9de
Sridhar Ratnakumar <srid@srid.ca>
2024-07-22 22:17:31 -0400

    init: Flesh out test for `om init`
    Basic tests for now.

--- crates/omnix-cli/tests/cli.rs
+++ crates/omnix-cli/tests/cli.rs
@@ -1,5 +1,6 @@
 use assert_cmd::Command;
 use predicates::prelude::*;
+use rexpect::spawn;
 
 /// `om --help` works
 #[test]
@@ -46,16 +47,34 @@ fn om_show_remote() -> anyhow::Result<()> {
 }
 
 /// `om init` runs and successfully initializes a template
-///
-/// NOTE: Test ignored due to https://github.com/mikaelmello/inquire/issues/71
-///
-/// Alternatively, we can pass all prompt data via CLI args, as JSON. But this
-/// may be overkill.
-#[ignore]
 #[test]
 fn om_init() -> anyhow::Result<()> {
     let temp_dir = assert_fs::TempDir::new().unwrap();
-    om()?.arg("init").arg(temp_dir.path()).assert().success();
+
+    // We can't use `om()`; see https://github.com/mikaelmello/inquire/issues/71
+    // om()?.arg("init").arg(temp_dir.path()).assert().success();
+    let om = assert_cmd::cargo::cargo_bin("om");
+
+    let mut p = spawn(
+        &format!("{:?} init {}", om, temp_dir.path().display()),
+        Some(30_000),
+    )?;
+    p.exp_string("Select a template")?;
+    p.send_line("haskell-template")?;
+    p.exp_string("Package Name")?;
+    p.send_line("foo")?;
+    p.exp_string("Author")?;
+    p.send_line("")?;
+    p.exp_string("VSCode support")?;
+    p.send_line("")?;
+    p.exp_string("Nix Template")?;
+    p.send_line("")?;
+    p.exp_string("GitHub Actions")?;
+    p.send_line("")?;
+
+    // TODO: Run the generated template, and compare output.
+    // Is there a better way of doing these checks? Property tests + ?
+
     temp_dir.close().unwrap();
     Ok(())
 }
