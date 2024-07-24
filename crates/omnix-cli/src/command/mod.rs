195434fb51c20e3ce8b1cd44e34315d9529178b9
Rohit Singh <rsrohitsingh682@gmail.com>
2024-07-24 22:22:57 +0530

    feat(omnix-cli): add shell completion

--- crates/omnix-cli/src/command/mod.rs
+++ crates/omnix-cli/src/command/mod.rs
@@ -1,9 +1,15 @@
 use clap::Subcommand;
 
+use crate::Args;
 pub mod ci;
 pub mod health;
 pub mod init;
 pub mod show;
+
+use anyhow::Ok;
+use clap::CommandFactory;
+use clap_complete::generate;
+use std::io;
 
 #[derive(Subcommand, Debug)]
 pub enum Command {
@@ -14,6 +20,12 @@ pub enum Command {
     CI(ci::CIConfig),
 
     Health(health::HealthConfig),
+
+    /// Generates shell completion scripts
+    Completion {
+        #[arg(value_enum)]
+        shell: clap_complete::Shell,
+    },
 }
 
 impl Command {
@@ -23,6 +35,11 @@ impl Command {
             Command::Init(config) => config.run().await,
             Command::CI(config) => config.run().await,
             Command::Health(config) => config.run().await,
+            Command::Completion { shell } => {
+                let mut cli = Args::command();
+                generate(*shell, &mut cli, "om", &mut io::stdout());
+                Ok(())
+            }
         }
     }
 }
