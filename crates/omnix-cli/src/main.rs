c15957d8b515e44de521e42eac7fc85301093c16
Sridhar Ratnakumar <srid@srid.ca>
2024-07-28 12:11:29 -0400

    test

--- crates/omnix-cli/src/main.rs
+++ crates/omnix-cli/src/main.rs
@@ -17,11 +17,10 @@ pub struct Args {
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    println!("Test");
     human_panic::setup_panic!();
     let args = Args::parse();
     let verbose = args.verbosity.log_level() > Some(clap_verbosity_flag::Level::Info);
     omnix::logging::setup_logging(&args.verbosity, !verbose);
-    tracing::debug!("Args: {:?}", args);
+    tracing::debug!("Args TEST: {:?}", args);
     args.command.run(args.verbosity).await
 }
