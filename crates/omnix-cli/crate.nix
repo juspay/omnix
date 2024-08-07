735a0d83cbc655461fd0960ac36a16f5788b664c
Sridhar Ratnakumar <srid@srid.ca>
2024-08-07 14:30:23 -0400

    flakreate: Refer to pristine source tree
    With `rust-project.src`, it refers only to Rust sources.
    
    This may possibly fix #208

--- crates/omnix-cli/crate.nix
+++ crates/omnix-cli/crate.nix
@@ -38,7 +38,7 @@ in
       OM_INIT_REGISTRY =
         lib.cleanSourceWith {
           name = "flakreate-registry";
-          src = rust-project.src + /crates/flakreate/registry;
+          src = flake.inputs.self + /crates/flakreate/registry;
         };
       NIX_FLAKE_SCHEMAS_BIN = lib.getExe pkgs.nix-flake-schemas;
       DEFAULT_FLAKE_SCHEMAS = inputs.flake-schemas;
