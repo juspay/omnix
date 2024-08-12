3c982af25e97f7e0af1fa6988a0c5316b360cdb2
Sridhar Ratnakumar <srid@srid.ca>
2024-08-12 16:04:42 -0400

    ci: Move test preparation step to `om ci`

--- nix/modules/om.nix
+++ nix/modules/om.nix
@@ -11,6 +11,11 @@ in
         omnix.dir = root;
         flakreate-registry.dir = root + /crates/flakreate/registry;
         doc.dir = root + /doc;
+
+        # Because the cargo tests invoking Nix doesn't pass github access tokens..
+        # To avoid GitHub rate limits during the integration test (which
+        # doesn't use the token)
+        cli-test-dep-cache.dir = root + /crates/omnix-cli/tests;
       };
       health.default = {
         nix-version.min-required = "2.16.0";
