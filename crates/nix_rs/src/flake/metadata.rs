d07b4eae37942233f254cb2d78a11070510e8b5b
Sridhar Ratnakumar <srid@srid.ca>
2024-08-19 15:12:28 -0400

    metadata.rs: add extra field, and document
    Just having `path` in struct can be confusing.

--- crates/nix_rs/src/flake/metadata.rs
+++ crates/nix_rs/src/flake/metadata.rs
@@ -1,29 +1,31 @@
-use crate::command::{NixCmd, NixCmdError};
-
 use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
 
 use super::url::FlakeUrl;
+use crate::command::{NixCmd, NixCmdError};
 
+/// Flake metadata
+///
+/// See [Nix doc](https://nix.dev/manual/nix/2.18/command-ref/new-cli/nix3-flake-metadata)
 #[derive(Serialize, Deserialize, Debug)]
 pub struct FlakeMetadata {
+    /// Original flake URL
+    #[serde(rename = "originalUrl")]
+    pub original_url: FlakeUrl,
+
+    /// Locally cached path for the flake.
     pub path: PathBuf,
 }
 
 impl FlakeMetadata {
-    /// Runs `nix flake metadata --json` for a given flake url in Rust
+    /// Runs `nix flake metadata --json` for a given flake url
     pub async fn from_nix(
         cmd: &NixCmd,
         flake_url: &FlakeUrl,
     ) -> Result<FlakeMetadata, NixCmdError> {
-        let metadata = cmd
-            .run_with_args_expecting_json::<FlakeMetadata>(&[
-                "flake",
-                "metadata",
-                "--json",
-                &flake_url.0,
-            ])
-            .await?;
-        Ok(metadata)
+        cmd.run_with_args_expecting_json::<FlakeMetadata>(&[
+            "flake", "metadata", "--json", flake_url,
+        ])
+        .await
     }
 }
