28c24a26ca7e84e133f2fab6c914b1fcf55b2980
Sridhar Ratnakumar <srid@srid.ca>
2024-08-27 18:57:01 -0400

    cli/test: Split into modules

---
+++ crates/omnix-cli/tests/command/show.rs
@@ -0,0 +1,29 @@
+use predicates::{prelude::*, str::contains};
+
+use super::core::om;
+
+/// `om show` runs, and succeeds for a local flake.
+#[test]
+fn om_show_local() -> anyhow::Result<()> {
+    om()?.arg("show").arg(".").assert().success().stdout(
+        contains("Packages")
+            .and(contains("Apps"))
+            .and(contains("Devshells"))
+            .and(contains("Checks")),
+    );
+    Ok(())
+}
+
+/// `om show` runs, and succeeds for a remote flake.
+#[test]
+fn om_show_remote() -> anyhow::Result<()> {
+    om()?
+        .arg("show")
+        .arg("github:srid/haskell-multi-nix/c85563721c388629fa9e538a1d97274861bc8321")
+        .assert()
+        .success()
+        .stdout(contains("bar").and(contains(
+            "github:srid/haskell-multi-nix/c85563721c388629fa9e538a1d97274861bc8321",
+        )));
+    Ok(())
+}
