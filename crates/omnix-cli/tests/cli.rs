54db7ae930152779c0af01bb4e89a2eb88f21bf1
Sridhar Ratnakumar <srid@srid.ca>
2024-08-27 18:21:13 -0400

    cli/test: use mod

--- crates/omnix-cli/tests/cli.rs
+++ crates/omnix-cli/tests/cli.rs
@@ -1,5 +1,5 @@
 use assert_cmd::Command;
-use predicates::prelude::*;
+use predicates::{prelude::*, str::contains};
 use rexpect::spawn;
 
 /// `om --help` works
@@ -9,82 +9,92 @@ fn om_help() -> anyhow::Result<()> {
     Ok(())
 }
 
-/// `om health` runs, and succeeds.
-#[test]
-fn om_health() -> anyhow::Result<()> {
-    om()?.arg("health").assert().success().stderr(
-        predicate::str::contains("All checks passed")
-            .or(predicate::str::contains("Required checks passed")),
-    );
-    Ok(())
+mod om_health_tests {
+    use super::*;
+    /// `om health` runs, and succeeds.
+    #[test]
+    fn om_health() -> anyhow::Result<()> {
+        om()?
+            .arg("health")
+            .assert()
+            .success()
+            .stderr(contains("All checks passed").or(contains("Required checks passed")));
+        Ok(())
+    }
 }
 
-/// `om show` runs, and succeeds for a local flake.
-#[test]
-fn om_show_local() -> anyhow::Result<()> {
-    om()?.arg("show").arg(".").assert().success().stdout(
-        predicate::str::contains("Packages")
-            .and(predicate::str::contains("Devshells"))
-            .and(predicate::str::contains("Checks")),
-    );
-    Ok(())
-}
+mod om_show_tests {
+    use super::*;
 
-/// `om show` runs, and succeeds for a remote flake.
-#[test]
-fn om_show_remote() -> anyhow::Result<()> {
-    om()?
-        .arg("show")
-        .arg("github:srid/haskell-multi-nix/c85563721c388629fa9e538a1d97274861bc8321")
-        .assert()
-        .success()
-        .stdout(
-            predicate::str::contains("bar").and(predicate::str::contains(
-                "github:srid/haskell-multi-nix/c85563721c388629fa9e538a1d97274861bc8321",
-            )),
+    /// `om show` runs, and succeeds for a local flake.
+    #[test]
+    fn om_show_local() -> anyhow::Result<()> {
+        om()?.arg("show").arg(".").assert().success().stdout(
+            contains("Packages")
+                .and(contains("Apps"))
+                .and(contains("Devshells"))
+                .and(contains("Checks")),
         );
-    Ok(())
+        Ok(())
+    }
+
+    /// `om show` runs, and succeeds for a remote flake.
+    #[test]
+    fn om_show_remote() -> anyhow::Result<()> {
+        om()?
+            .arg("show")
+            .arg("github:srid/haskell-multi-nix/c85563721c388629fa9e538a1d97274861bc8321")
+            .assert()
+            .success()
+            .stdout(contains("bar").and(contains(
+                "github:srid/haskell-multi-nix/c85563721c388629fa9e538a1d97274861bc8321",
+            )));
+        Ok(())
+    }
 }
 
-/// `om init` runs and successfully initializes a template
-#[test]
-fn om_init() -> anyhow::Result<()> {
-    let temp_dir = assert_fs::TempDir::new().unwrap();
+mod om_init_tests {
+    use super::*;
+    /// `om init` runs and successfully initializes a template
+    #[test]
+    fn om_init() -> anyhow::Result<()> {
+        let temp_dir = assert_fs::TempDir::new().unwrap();
 
-    // We can't use `om()`; see https://github.com/mikaelmello/inquire/issues/71
-    // om()?.arg("init").arg(temp_dir.path()).assert().success();
-    let om = assert_cmd::cargo::cargo_bin("om");
+        // We can't use `om()`; see https://github.com/mikaelmello/inquire/issues/71
+        // om()?.arg("init").arg(temp_dir.path()).assert().success();
+        let om = assert_cmd::cargo::cargo_bin("om");
 
-    let mut p = spawn(
-        &format!("{:?} init {}", om, temp_dir.path().display()),
-        Some(30_000),
-    )?;
-    p.exp_string("Select a template")?;
-    p.send_line("haskell-template")?;
-    p.exp_string("Author")?;
-    p.send_line("")?;
-    p.exp_string("Package Name")?;
-    p.send_line("foo")?;
-    p.exp_string("VSCode support")?;
-    p.send_line("")?;
-    p.exp_string("Nix Template")?;
-    p.send_line("")?;
-    p.exp_string("GitHub Actions")?;
-    p.send_line("")?;
-    p.exp_eof()?;
+        let mut p = spawn(
+            &format!("{:?} init {}", om, temp_dir.path().display()),
+            Some(30_000),
+        )?;
+        p.exp_string("Select a template")?;
+        p.send_line("haskell-template")?;
+        p.exp_string("Author")?;
+        p.send_line("")?;
+        p.exp_string("Package Name")?;
+        p.send_line("foo")?;
+        p.exp_string("VSCode support")?;
+        p.send_line("")?;
+        p.exp_string("Nix Template")?;
+        p.send_line("")?;
+        p.exp_string("GitHub Actions")?;
+        p.send_line("")?;
+        p.exp_eof()?;
 
-    // Run the generated template, and compare output.
-    // Is there a better way of doing these checks? Property tests + ?
-    // TODO: github token?
-    Command::new("nix")
-        .arg("run")
-        .arg(format!("path:{}#foo", &temp_dir.path().display()))
-        .assert()
-        .success()
-        .stdout(predicate::str::contains("Hello"));
+        // Run the generated template, and compare output.
+        // Is there a better way of doing these checks? Property tests + ?
+        // TODO: github token?
+        Command::new("nix")
+            .arg("run")
+            .arg(format!("path:{}#foo", &temp_dir.path().display()))
+            .assert()
+            .success()
+            .stdout(contains("Hello"));
 
-    temp_dir.close().unwrap();
-    Ok(())
+        temp_dir.close().unwrap();
+        Ok(())
+    }
 }
 
 mod om_ci_tests {
