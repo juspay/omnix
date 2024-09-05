bebe7a35d0f0470980dc1b74e506ded47880f251
Sridhar Ratnakumar <srid@srid.ca>
2024-09-04 22:30:07 -0400

    no exe

--- crates/omnix-init/src/main.rs
+++
@@ -1,8 +0,0 @@
-use std::{collections::HashMap, path::Path};
-
-use omnix_init::core::initialize_template;
-
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    initialize_template(Path::new("/tmp/init"), None, &HashMap::new(), false).await
-}
