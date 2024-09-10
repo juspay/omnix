23ec8e9c6702c6026d37a041d6f5ac68321274dd
Sridhar Ratnakumar <srid@srid.ca>
2024-09-09 22:00:56 -0400

    test: om init test types

--- crates/omnix-cli/tests/command/init.rs
+++ crates/omnix-cli/tests/command/init.rs
@@ -6,46 +6,77 @@ use predicates::str::contains;
 /// `om init` runs and successfully initializes a template
 #[test]
 fn om_init() -> anyhow::Result<()> {
-    let temp_dir = assert_fs::TempDir::new().unwrap();
-
-    let template_name = "haskell-template";
-    let default_params = r#"{"package-name": "foo", "author": "John", "vscode": false }"#;
-
-    om()?
-        .args([
-            "init",
-            "-o",
-            &temp_dir.to_string_lossy(),
-            template_name,
-            "--non-interactive",
-            "--params",
-            default_params,
-        ])
-        .assert()
-        .success();
-
-    // File inclusion checks
-    // Fail if .vscode/ directory exists in temp_dir
-    assert!(!temp_dir.child(".vscode").exists());
-    // .github/ must exist (template includes by default)
-    assert!(temp_dir.child(".github").exists());
-    // .github must have files inside it
-    assert!(temp_dir
-        .child(".github")
-        .child("workflows")
-        .child("ci.yaml")
-        .exists());
-
-    // Run the generated template, and compare output.
-    // Is there a better way of doing these checks? Property tests + ?
-    // TODO: github token?
-    Command::new("nix")
-        .arg("run")
-        .arg(format!("path:{}#foo", &temp_dir.path().display()))
-        .assert()
-        .success()
-        .stdout(contains("from foo"));
-
-    temp_dir.close().unwrap();
+    for test in om_init_tests() {
+        test.run_test();
+    }
     Ok(())
+}
+
+fn om_init_tests() -> Vec<OmInitTest> {
+    vec![OmInitTest {
+        template_name: "haskell-template",
+        default_params: r#"{"package-name": "foo", "author": "John", "vscode": false }"#,
+        assert_path_exists: vec![".github/workflows/ci.yaml"],
+        assert_path_not_exists: vec![".vscode"],
+        assert_nix_run_output_contains: "from foo",
+    }]
+}
+
+/// A test for a single template
+struct OmInitTest {
+    /// The template name to pass to `om init`
+    template_name: &'static str,
+    /// The --default-params to pass to `om init`
+    default_params: &'static str,
+    /// These paths must exist in the output directory
+    assert_path_exists: Vec<&'static str>,
+    /// These paths must not exist
+    assert_path_not_exists: Vec<&'static str>,
+    /// `nix run` should produce a certain output (contains this string)
+    assert_nix_run_output_contains: &'static str,
+}
+
+impl OmInitTest {
+    /// Run this test on a temporary directory
+    fn run_test(&self) -> anyhow::Result<()> {
+        let temp_dir = assert_fs::TempDir::new().unwrap();
+        om()?
+            .args([
+                "init",
+                "-o",
+                &temp_dir.to_string_lossy(),
+                self.template_name,
+                "--non-interactive",
+                "--params",
+                self.default_params,
+            ])
+            .assert()
+            .success();
+
+        for path in &self.assert_path_exists {
+            assert!(
+                temp_dir.child(path).exists(),
+                "Expected path to exist: {}",
+                path
+            );
+        }
+        for path in &self.assert_path_not_exists {
+            assert!(
+                !temp_dir.child(path).exists(),
+                "Expected path to not exist: {}",
+                path
+            );
+        }
+
+        // Run the generated template, and compare output.
+        // TODO: github token?
+        Command::new("nix")
+            .arg("run")
+            .arg(format!("path:{}", &temp_dir.path().display()))
+            .assert()
+            .success()
+            .stdout(contains(self.assert_nix_run_output_contains));
+        temp_dir.close().unwrap();
+        Ok(())
+    }
 }
