90808735f2ba51bf51797c537ce9b9e2723076b5
Sridhar Ratnakumar <srid@srid.ca>
2024-09-19 14:39:34 -0400

    omnix-init: Use the new `nix_eval` in `load_templates`

--- crates/omnix-init/src/config.rs
+++ crates/omnix-init/src/config.rs
@@ -2,7 +2,7 @@ use std::{collections::HashMap, path::PathBuf, sync::LazyLock};
 
 use nix_rs::{
     command::NixCmd,
-    flake::{eval::nix_eval_attr, url::FlakeUrl},
+    flake::{command::FlakeOptions, eval::nix_eval, url::FlakeUrl},
 };
 
 use crate::template::Template;
@@ -14,13 +14,13 @@ static REGISTRY: LazyLock<FlakeUrl> =
 /// The `om.templates` config in flake
 pub type TemplatesConfig = HashMap<String, Template>;
 
-/// Load templates from our builtin registry
+/// Load templates from our builtin registry `REGISTRY`
 pub async fn load_templates() -> anyhow::Result<TemplatesConfig> {
-    let registry = REGISTRY.clone();
-    match nix_eval_attr::<TemplatesConfig>(&NixCmd::default(), &registry.with_attr("om.templates"))
-        .await?
-    {
-        Some(v) => Ok(v),
-        None => Ok(HashMap::new()),
-    }
+    let v = nix_eval::<TemplatesConfig>(
+        &NixCmd::default(),
+        &FlakeOptions::default(),
+        &REGISTRY.with_attr("om.templates"),
+    )
+    .await?;
+    Ok(v)
 }
