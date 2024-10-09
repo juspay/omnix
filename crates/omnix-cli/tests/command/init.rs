db9b56cb768693ee77aca0e6b51611282993ff77
Sridhar Ratnakumar <srid@srid.ca>
2024-10-09 16:37:29 -0400

    init/test: Adopt logic from run_tests
    We were missing the system filtering

--- crates/omnix-cli/tests/command/init.rs
+++ crates/omnix-cli/tests/command/init.rs
@@ -1,24 +1,35 @@
+use nix_rs::config::NixConfig;
 use omnix_init::registry::BUILTIN_REGISTRY;
 
 /// `om init` runs and successfully initializes a template
 #[tokio::test]
 async fn om_init() -> anyhow::Result<()> {
     let registry = BUILTIN_REGISTRY.clone();
+    let cfg = NixConfig::get().await.as_ref()?;
+    let current_system = &cfg.system.value;
     for url in registry.0.values() {
+        // TODO: Refactor(DRY) with src/core.rs:run_tests
         println!("🕍 Testing template: {}", url);
         let templates = omnix_init::config::load_templates(url).await?;
         for template in templates {
             let tests = &template.template.tests;
             for (name, test) in tests {
-                println!(
-                    "🧪 [{}#{}] Running test: {}",
-                    url, template.template_name, name
-                );
-                test.run_test(
-                    &url.with_attr(&format!("{}.{}", template.template_name, name)),
-                    &template,
-                )
-                .await?;
+                if test.can_run_on(current_system) {
+                    println!(
+                        "🧪 [{}#{}] Running test: {}",
+                        url, template.template_name, name
+                    );
+                    test.run_test(
+                        &url.with_attr(&format!("{}.{}", template.template_name, name)),
+                        &template,
+                    )
+                    .await?;
+                } else {
+                    println!(
+                        "⚠️ Skipping test: {} (cannot run on {})",
+                        name, current_system
+                    );
+                }
             }
         }
     }
