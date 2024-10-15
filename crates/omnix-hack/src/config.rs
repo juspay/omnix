a16505241892797f0caf5386444384b9d1eceb6d
Sridhar Ratnakumar <srid@srid.ca>
2024-10-14 21:55:04 -0400

    ref/doc

--- crates/omnix-hack/src/config.rs
+++ crates/omnix-hack/src/config.rs
@@ -1,14 +1,14 @@
-use nix_rs::{
-    command::NixCmd,
-    flake::{command::FlakeOptions, url::FlakeUrl},
-};
-use omnix_common::config::OmConfig;
 use serde::Deserialize;
+
+use nix_rs::{command::NixCmd, flake::url::FlakeUrl};
+use omnix_common::config::OmConfig;
+
+use crate::readme::Readme;
 
 #[derive(Debug, Deserialize, Clone)]
 pub struct HackConfig {
     pub cache: CacheConfig,
-    pub readme: ReadmeConfig,
+    pub readme: Readme,
 }
 
 #[derive(Debug, Deserialize, Clone)]
@@ -18,21 +18,18 @@ pub struct CacheConfig {
 
 #[derive(Debug, Deserialize, Clone)]
 pub struct CachixConfig {
+    /// If enabled, configure environment to use the cache.
     pub enable: bool,
+    /// Name of the cachix cache (https://<name>.cachix.org)
     pub name: String,
-    /// The read-only auth token to use if a private cache
+    /// The read-only auth token to use if this is a private cache
+    ///
+    /// If provided, will run `cachix authtoken <auth_token>`.
     pub auth_token: Option<String>,
 }
 
-#[derive(Debug, Deserialize, Clone)]
-pub struct ReadmeConfig(pub String);
-
 impl HackConfig {
     pub async fn from_flake(url: &FlakeUrl) -> anyhow::Result<Self> {
-        let _opts = FlakeOptions {
-            refresh: true,
-            ..Default::default()
-        };
         let v = OmConfig::<Self>::from_flake_url(NixCmd::get().await, url, &["om.hack"])
             .await?
             .config;
--- crates/omnix-hack/src/lib.rs
+++ crates/omnix-hack/src/lib.rs
@@ -1,2 +1,3 @@
 pub mod config;
 pub mod core;
+pub mod readme;
