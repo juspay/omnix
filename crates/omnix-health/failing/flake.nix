22442f4c3907eae53f0fa0289d31fa359a2073c8
Sridhar Ratnakumar <srid@srid.ca>
2024-10-22 15:40:43 -0400

    bacon: Add health failing watch

--- crates/omnix-health/failing/flake.nix
+++ crates/omnix-health/failing/flake.nix
@@ -1,11 +1,11 @@
-# Just a flake.nix to configure nix-health to fail all possible checks
+# Just a flake.nix to configure omnix-health to fail all possible checks
 #
 # Used for testing purposes; run as:
-#   just watch-nix-health ./crates/omnix-health/failing/
+#   cargo run -p omnix-cli health ./crates/omnix-health/failing
 
 {
   outputs = _: {
-    nix-health.default = {
+    om.health.default = {
       caches.required = [ "https://unknown.cachix.org" "https://example.com" ];
       nix-version.min-required = "9.99.99";
       system = {
