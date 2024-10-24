9e4d2c4de7d627a8ed17d8d8a852ed6d430cfb14
Sridhar Ratnakumar <srid@srid.ca>
2024-10-24 11:34:14 -0400

    hack: Implement pre-shell and post-shell stages

--- crates/omnix-cli/src/command/hack.rs
+++ crates/omnix-cli/src/command/hack.rs
@@ -8,11 +8,32 @@ pub struct HackCommand {
     /// Directory of the project
     #[arg(name = "DIR", default_value = ".")]
     dir: PathBuf,
+
+    // TODO: Implement these options
+    // om hack --mode-before
+    // om hack --mode-after-success
+    #[arg(long, value_enum)]
+    stage: Option<Stage>,
+}
+
+/// The stage to run in
+#[derive(clap::ValueEnum, Debug, Clone)]
+enum Stage {
+    /// Stage before Nix shell is invoked.
+    PreShell,
+
+    /// Stage after Nix shell is successfully invoked.
+    PostShell,
 }
 
 impl HackCommand {
     pub async fn run(&self) -> anyhow::Result<()> {
-        omnix_hack::core::hack_on(&self.dir).await?;
+        let prj = omnix_hack::core::Project::new(&self.dir).await?;
+        match self.stage {
+            Some(Stage::PreShell) => omnix_hack::core::hack_on_pre_shell(&prj).await?,
+            Some(Stage::PostShell) => omnix_hack::core::hack_on_post_shell(&prj).await?,
+            None => omnix_hack::core::hack_on(&prj).await?,
+        }
         Ok(())
     }
 }
--- crates/omnix-hack/src/core.rs
+++ crates/omnix-hack/src/core.rs
@@ -1,5 +1,5 @@
 use anyhow::Context;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 use nix_rs::{flake::url::FlakeUrl, info::NixInfo};
 use omnix_common::markdown::print_markdown;
@@ -7,15 +7,31 @@ use omnix_health::{traits::Checkable, NixHealth};
 
 use crate::config::HackConfig;
 
-pub async fn hack_on(dir: &Path) -> anyhow::Result<()> {
-    let dir = dir.canonicalize()?;
-    let here_flake: FlakeUrl = Into::<FlakeUrl>::into(dir.as_ref());
-    let cfg = HackConfig::from_flake(&here_flake).await?;
+pub struct Project {
+    pub dir: PathBuf,
+    pub flake: FlakeUrl,
+    pub cfg: HackConfig,
+}
 
-    // TODO: cachix check
+impl Project {
+    pub async fn new(dir: &Path) -> anyhow::Result<Self> {
+        let dir = dir.canonicalize()?;
+        let flake: FlakeUrl = Into::<FlakeUrl>::into(dir.as_ref());
+        let cfg = HackConfig::from_flake(&flake).await?;
+        Ok(Self { dir, flake, cfg })
+    }
+}
 
+pub async fn hack_on(prj: &Project) -> anyhow::Result<()> {
+    hack_on_pre_shell(prj).await?;
+    hack_on_post_shell(prj).await?;
+    Ok(())
+}
+
+pub async fn hack_on_pre_shell(prj: &Project) -> anyhow::Result<()> {
     // Run relevant `om health` checks
-    let health = NixHealth::from_flake(&here_flake).await?;
+    tracing::info!("om hack: Running pre-shell checks");
+    let health = NixHealth::from_flake(&prj.flake).await?;
     let nix_info = NixInfo::get()
         .await
         .as_ref()
@@ -29,7 +45,7 @@ pub async fn hack_on(dir: &Path) -> anyhow::Result<()> {
         &health.caches,
     ];
     for check_kind in relevant_checks.into_iter() {
-        for check in check_kind.check(nix_info, Some(&here_flake)) {
+        for check in check_kind.check(nix_info, Some(&prj.flake)) {
             if !check.result.green() {
                 check.tracing_log().await?;
                 if !check.result.green() && check.required {
@@ -42,10 +58,11 @@ pub async fn hack_on(dir: &Path) -> anyhow::Result<()> {
     if !health.caches.required.is_empty() {
         // TODO: Auto-resolve some problems; like running 'cachix use' automatically
     };
-    tracing::info!("Healthy");
+    Ok(())
+}
 
+pub async fn hack_on_post_shell(prj: &Project) -> anyhow::Result<()> {
     eprintln!();
-    print_markdown(&dir, &cfg.readme.get_markdown()).await?;
-
+    print_markdown(&prj.dir, &prj.cfg.readme.get_markdown()).await?;
     Ok(())
 }
