831f2dfadbdf0c891fd88f6e219754d5809f8382
Sridhar Ratnakumar <srid@srid.ca>
2024-10-25 16:16:32 -0400

    hack: Improve logging a bit

--- crates/nix_rs/src/command.rs
+++ crates/nix_rs/src/command.rs
@@ -74,14 +74,14 @@ static NIXCMD: OnceCell<NixCmd> = OnceCell::const_new();
 /// rest of the instrumentation parameters.
 #[instrument(name = "command")]
 pub fn trace_cmd(cmd: &tokio::process::Command) {
-    trace_cmd_with("〕", cmd);
+    trace_cmd_with("❄️ ", cmd);
 }
 
 /// Like [trace_cmd] but with a custom icon
 #[instrument(name = "command")]
 pub fn trace_cmd_with(icon: &str, cmd: &tokio::process::Command) {
     use colored::Colorize;
-    tracing::info!("{} {}️", icon, to_cli(cmd).dimmed());
+    tracing::info!("{}", format!("{} {}️", icon, to_cli(cmd)).dimmed());
 }
 
 impl NixCmd {
