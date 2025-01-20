fd04a6a6e50b21c7e5043aab9ead0156d1781e0f
Sridhar Ratnakumar <srid@srid.ca>
2025-01-20 12:45:23 -0500

    nix_rs/metadata: Account for cyclic inputs
    https://github.com/NixOS/nix/issues/7807
    
    https://github.com/NixOS/nix/blob/ac31767c5725be8d363884a0e982c72af868590f/flake.nix#L18

--- crates/nix_rs/src/flake/metadata/flake.nix
+++ crates/nix_rs/src/flake/metadata/flake.nix
@@ -12,14 +12,21 @@
           # *All* nested inputs are flattened into a single list of inputs.
           inputs =
             let
-              inputsFor = prefix: f:
-                lib.concatLists (lib.mapAttrsToList
-                  (k: v: [{ name = "${prefix}__${k}"; path = v.outPath; }] ++
-                    (lib.optionals (lib.hasAttr "inputs" v))
-                      (inputsFor k v))
-                  f.inputs);
+              inputsFor = visited: prefix: f:
+                let
+                  here = builtins.unsafeDiscardStringContext "${f.outPath}";
+                in
+                # Keep track of visited nodes to workaround a nasty Nix design wart that leads to infinite recursion otherwise.
+                  # https://github.com/NixOS/nix/issues/7807
+                  # https://github.com/juspay/omnix/pull/389
+                lib.optionals (!lib.hasAttr here visited)
+                  (lib.concatLists (lib.mapAttrsToList
+                    (k: v: [{ name = "${prefix}__${k}"; path = v.outPath; }] ++
+                      (lib.optionals (lib.hasAttr "inputs" v))
+                        (inputsFor (visited // { "${here}" = true; }) "${prefix}/${k}" v))
+                    f.inputs));
             in
-            inputsFor "flake" inputs.flake;
+            inputsFor { } "flake" inputs.flake;
           flake = inputs.flake.outPath;
         });
       };
