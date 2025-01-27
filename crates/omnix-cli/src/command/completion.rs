cab9f66e6dddeffed7fcaec647249e3bd137c321
Sridhar Ratnakumar <srid@srid.ca>
2025-01-27 16:21:19 -0500

    cli: Add nushell completion

--- crates/omnix-cli/src/command/completion.rs
+++ crates/omnix-cli/src/command/completion.rs
@@ -1,13 +1,12 @@
 use clap::CommandFactory;
 use clap::Parser;
 use clap_complete::generate;
-use clap_complete::Shell;
 
 /// Generates shell completion scripts
 #[derive(Parser, Debug)]
 pub struct CompletionCommand {
     #[arg(value_enum)]
-    shell: clap_complete::Shell,
+    shell: Shell2,
 }
 
 impl CompletionCommand {
@@ -17,11 +16,36 @@ impl CompletionCommand {
     }
 }
 
-fn generate_completion(shell: Shell) {
-    generate(
-        shell,
-        &mut crate::args::Args::command(),
-        "om",
-        &mut std::io::stdout(),
-    )
+/// Like `clap::Shell`, but with an additional variant for Nushell
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, clap::ValueEnum)]
+#[non_exhaustive]
+enum Shell2 {
+    Bash,
+    Elvish,
+    Fish,
+    PowerShell,
+    Zsh,
+    Nushell,
+}
+
+impl Shell2 {
+    fn nushell_or(self) -> Result<clap_complete::Shell, clap_complete_nushell::Nushell> {
+        match self {
+            Shell2::Nushell => Err(clap_complete_nushell::Nushell),
+            Shell2::Bash => Ok(clap_complete::Shell::Bash),
+            Shell2::Elvish => Ok(clap_complete::Shell::Elvish),
+            Shell2::Fish => Ok(clap_complete::Shell::Fish),
+            Shell2::PowerShell => Ok(clap_complete::Shell::PowerShell),
+            Shell2::Zsh => Ok(clap_complete::Shell::Zsh),
+        }
+    }
+}
+
+fn generate_completion(shell: Shell2) {
+    let cmd = &mut crate::args::Args::command();
+    let out = &mut std::io::stdout();
+    match shell.nushell_or() {
+        Ok(shell) => generate(shell, cmd, "om", out),
+        Err(shell) => generate(shell, cmd, "om", out),
+    }
 }
