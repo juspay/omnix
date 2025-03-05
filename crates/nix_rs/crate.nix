87bd8dd734c85c61053b4ddb42b3c5f5578e8d37
shivaraj-bh <me@shivaraj-bh.in>
2025-03-05 19:19:31 +0530

    fix mac build

--- crates/nix_rs/crate.nix
+++ crates/nix_rs/crate.nix
@@ -1,9 +1,15 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   # Autowiring `crate` so that the tests are run in nix sandbox when `om ci` is used
   autoWire = [ "crate" ];
   crane = {
     args = {
+      buildInputs = lib.optionals pkgs.stdenv.isDarwin (
+        with pkgs.apple_sdk_frameworks; [
+          IOKit
+          SystemConfiguration
+        ]
+      );
       nativeBuildInputs = with pkgs; [
         nix # Tests need nix cli
       ];
--- crates/omnix-common/crate.nix
+++ crates/omnix-common/crate.nix
@@ -1,9 +1,17 @@
-{ lib, ... }:
+{ pkgs, lib, ... }:
 {
   # Autowiring `crate` so that the tests are run in nix sandbox when `om ci` is used
   autoWire = [ "crate" ];
-  crane.args.cargoTestExtraArgs = "-- " + (lib.concatStringsSep " " [
-    # requires networking
-    "--skip=config::test_get_omconfig_from_remote_flake_with_attr"
-  ]);
+  crane.args = {
+    buildInputs = lib.optionals pkgs.stdenv.isDarwin (
+      with pkgs.apple_sdk_frameworks; [
+        IOKit
+        SystemConfiguration
+      ]
+    );
+    cargoTestExtraArgs = "-- " + (lib.concatStringsSep " " [
+      # requires networking
+      "--skip=config::test_get_omconfig_from_remote_flake_with_attr"
+    ]);
+  };
 }
