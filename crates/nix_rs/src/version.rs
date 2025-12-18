0a0dfd462c182e2421cfa8f0d3a511003ab810a0
Andrei Cravtov <the.andrei.cravtov@gmail.com>
2025-12-18 16:54:11 +0000

    fix(bug): version parser tolerates optional `+{number}` at the end (#489)
    Resolves:  https://github.com/juspay/omnix/issues/488
    By applying @HarrisonCentner's fix ->
    https://github.com/juspay/omnix/issues/488#issuecomment-3668030239 (so
    full credit to @HarrisonCentner).

--- crates/nix_rs/src/version.rs
+++ crates/nix_rs/src/version.rs
@@ -43,7 +43,7 @@ impl FromStr for NixVersion {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         // NOTE: The parser is lenient in allowing pure nix version (produced
         // by [Display] instance), so as to work with serde_with instances.
-        let re = Regex::new(r"(?:nix \(Nix\) )?(\d+)\.(\d+)\.(\d+)$")?;
+        let re = Regex::new(r"(?:nix \(Nix\) )?(\d+)\.(\d+)\.(\d+)(?:\+(\d+))?$")?;
 
         let captures = re.captures(s).ok_or(BadNixVersion::Command)?;
         let major = captures[1].parse::<u32>()?;
